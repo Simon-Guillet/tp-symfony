ee686155b7d5e51edd5f5193cb9e6f49a7efdb16
Simon <simon.guillet@free.fr>
2021-10-27 16:36:09 +0200

    1.1

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,68 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
+        $articles =$this->getDoctrine()->getRepository(Article::class)->findAll();
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        $articles =$this->getDoctrine()->getRepository(Article::class)->findAll();
+        return $this->render('main/about.html.twig');
+    }
+    /**
+     * @Route("/post", name="post")
+     */
+    public function post(): Response
+    {
+        return $this->render('main/post.html.twig');
+    }
+    /**
+     * @Route("/add", name="form")
+     */
+    public function form(Request $request): Response
+    {
+        $post = new Article();
+        $post->setTitle('Titre');
+        $post->setArticle('Article');
+        $post->setAuthor('Auteur');
+        $post->setCreatedAt(new \DateTime('now'));
+
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class)
+            ->add('article', TextType::class)
+            ->add('author', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+        }
+
+        return $this->render('main/form.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $article;
+
+    /**
+     * @ORM\Column(type="string", length=70)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->article;
+    }
+
+    public function setArticle(string $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
